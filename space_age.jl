c6cd4f563441c46f8e88df620bcf0405171c903b
Tim-Gaede <TimGaede@gmail.com>
2019-10-01 01:39:31 -0400

    Add files via upload

---
+++ space_age.jl
@@ -0,0 +1,38 @@
+module SpaceAge
+    export on_mercury, on_venus, on_earth, on_mars,
+           on_jupiter, on_saturn, on_uranus, on_neptune
+
+
+    function on_mercury(seconds::Number)
+
+    end
+
+    function on_venus(seconds::Number) 
+
+    end
+
+    function on_earth(seconds::Number)
+
+    end
+
+    function on_mars(seconds::Number)
+
+    end
+
+
+    function on_jupiter(seconds::Number)
+
+    end
+
+    function on_saturn(seconds::Number)
+
+    end
+
+    function on_uranus(seconds::Number)
+
+    end
+
+    function on_neptune(seconds::Number)
+
+    end
+end
